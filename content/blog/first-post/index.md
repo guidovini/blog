5363d50ac5e5b30757a8157f6bfb1ede779354bb
Guido Santillan <guidosantillan01@gmail.com>
2019-04-11 18:15:37 -0500

    Initial commit

---
+++ content/blog/first-post/index.md
@@ -0,0 +1,6 @@
+---
+title: My First post :)
+date: "2019-04-11T18:10:03.284Z"
+---
+
+Hey y'all, this is my awesome new blog!
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `GuiD Blog`,
+    author: `Guido Santillan`,
+    description: `A simple blog.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
