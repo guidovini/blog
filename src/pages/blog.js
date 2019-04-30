fd4800de0c661aed28a9ff732ce430aa4c0cef24
Guido Santillan <guidosantillan01@gmail.com>
2019-04-30 18:14:23 -0500

    List all blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  const posts = data.allMarkdownRemark.edges
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {posts.map(post => {
+          return (
+            <li>
+              <h2>{post.node.frontmatter.title}</h2>
+              <p>{post.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
+      
     </Layout>
   )
 }
