db5fd4d6919fb26f748c243e38463eb474faf88a
Guido Santillan <guidosantillan01@gmail.com>
2019-04-30 18:14:51 -0500

    Setup createPages and onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,12 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
+  // ! const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  const blogPost = path.resolve(`./src/templates/blog.js`)
+  const result = await graphql(
     `
       {
         allMarkdownRemark(
@@ -25,31 +26,31 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
-    }
+  )
+  
+  if (result.errors) {
+    throw result.errors
+  }
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+  // Create blog posts pages.
+  const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
+    createPage({
+      path: `/blog${post.node.fields.slug}`,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
     })
-
-    return null
   })
+
+  return null
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
