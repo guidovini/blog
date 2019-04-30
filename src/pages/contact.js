0ef024029b4edc1d9df7d2332059e7b694542bb0
Guido Santillan <guidosantillan01@gmail.com>
2019-04-30 15:15:21 -0500

    Add main pages

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+      <p>This is the contact page</p>
+    </Layout>
+  )
+}
+
+export default ContactPage
