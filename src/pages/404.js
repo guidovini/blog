91c2fb3a3949b77f9c3c9b9ffa3890482251430c
Guido Santillan <guidosantillan01@gmail.com>
2019-04-30 21:01:47 -0500

    Add custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,32 +1,17 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Head from '../components/head'
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+const NotFound = () => {
+  return (
+    <Layout>
+      <Head title="404" />
+      <h1>Page not found</h1>
+      <p><Link to='/'>Head home</Link></p>
+    </Layout>
+  )
 }
 
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default NotFound
