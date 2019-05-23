03dd68e373c834f39e38acf1178555d62b832322
Guido Santillan <guidosantillan01@gmail.com>
2019-05-23 13:26:46 -0500

    Add styles to the blog

--- src/components/header.js
+++ src/components/header.js
@@ -17,14 +17,12 @@ const Header = (props) => {
 
   return (
     <header className={headerStyles.header}>
-      <Link to='/' className={headerStyles.logo}><img src={logo} alt={"logo"}/></Link>
-      <nav className={headerStyles.nav}>  
+      <nav>  
         <ul className={headerStyles.navList}>
-        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/projects'>Projects</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Articles</Link></li>
-          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact</Link></li>
         </ul>
       </nav>
     </header>
