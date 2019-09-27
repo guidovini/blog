995c307e143537907cf8e2999093ecec726d798b
Guido Santillan <guidosantillan01@gmail.com>
2019-09-27 20:08:08 +0200

    Separate projects data from main file. Change projects color to green.

---
+++ src/data/projects.js
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    title: "Syder Ideas",
+    description: "Store your ideas in an intuitive way.",
+    tech: ["js", "react", "redux", "node", "express", "postgresql"],
+    category: "dev",
+    imageUrl: "../static/projects/dev/syder-ideas-main.png",
+    githubUrl: "https://github.com/guidosantillan01/syder-ideas",
+    liveUrl: "https://syder-ideas-client.herokuapp.com/",
+  },
+  {
+    title: "Expofranquicias",
+    description: "Find LatinAmerican franchises here.",
+    tech: ["react", "gatsby", "graphql"],
+    category: "dev",
+    imageUrl: "../static/projects/dev/expofranquicias-main.png",
+    githubUrl: undefined,
+    liveUrl: "https://expofranquiciasecuador.com/franquicias-disponibles",
+  },
+  {
+    title: "Robot controlled by Human Motion",
+    description: "It uses KINECT to capture your movement.",
+    tech: ["arduino", "processing"],
+    category: "mechatronics",
+    imageUrl: "../static/projects/mechatronics/robot-main.png",
+    githubUrl: undefined,
+    liveUrl: undefined,
+  },
+  {
+    title: "Image Detection of Drivers",
+    description: "Kaggle",
+    tech: ["pytorch", "python"],
+    category: "AI",
+    imageUrl: "../static/projects/others/deep-learning-main.png",
+    githubUrl: "https://github.com/guidosantillan01/pytorch-distracted-drivers",
+    liveUrl: undefined,
+  },
+  {
+    title: "Flower Image Detection",
+    description: "Kaggle",
+    tech: ["pytorch", "python"],
+    category: "AI",
+    imageUrl: "../static/projects/others/deep-learning-main.png",
+    githubUrl: "https://github.com/guidosantillan01/pytorch-distracted-drivers",
+    liveUrl: undefined,
+  },
+]
+
+export { projects as default }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,44 +5,7 @@ import Head from "../components/head"
 import Project from "../components/project"
 import ComingSoon from "../components/coming-soon"
 
-const projects = [
-  {
-    title: "Syder Ideas",
-    description: "Store your ideas in an intuitive way.",
-    tech: ["js", "react", "node", "express"],
-    category: "dev",
-    imageUrl: "../static/projects/dev/syder-ideas-main.png",
-    githubUrl: "https://github.com/guidosantillan01/syder-ideas",
-    liveUrl: "https://syder-ideas-client.herokuapp.com/",
-  },
-  {
-    title: "Expofranquicias",
-    description: "Find LatinAmerican franchises here.",
-    tech: ["react", "gatsby", "graphql"],
-    category: "dev",
-    imageUrl: "../static/projects/dev/expofranquicias-main.png",
-    githubUrl: undefined,
-    liveUrl: "https://expofranquiciasecuador.com/franquicias-disponibles",
-  },
-  {
-    title: "Robot controlled by Human Motion",
-    description: "It uses KINECT to capture your movement.",
-    tech: ["arduino", "processing"],
-    category: "mechatronics",
-    imageUrl: "../static/projects/mechatronics/robot-main.png",
-    githubUrl: undefined,
-    liveUrl: undefined,
-  },
-  {
-    title: "Image Detection of Drivers",
-    description: "Kaggle",
-    tech: ["pytorch", "python"],
-    category: "AI",
-    imageUrl: "../static/projects/others/deep-learning-main.png",
-    githubUrl: "https://github.com/guidosantillan01/pytorch-distracted-drivers",
-    liveUrl: undefined,
-  },
-]
+import projects from "../data/projects"
 
 const renderProjects = true
 
